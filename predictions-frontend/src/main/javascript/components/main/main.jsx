2f8230dc1b17f7883849df14a636548a358853f7
Martin Fanta <mafanta@cisco.com>
2017-03-24 21:04:52 -0400

    Get the user name from the backend.

--- predictions-frontend/src/main/javascript/components/main/main.jsx
+++ predictions-frontend/src/main/javascript/components/main/main.jsx
@@ -2,21 +2,8 @@ import React from 'react';
 
 export default class MainComponent extends React.Component {
 
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      userName: 'Martin'
-    };
-  }
-
   render() {
-    return <div>Hello {this.state.userName}!</div>;
-  }
-
-  componentDidMount() {
-    this.setState({
-      userName: 'Tonda'
-    });
+    return <div>Hello {this.props.userName}!</div>;
   }
 }
 
--- predictions-frontend/src/main/javascript/root.jsx
+++ predictions-frontend/src/main/javascript/root.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_ROOT = 'http://localhost:8080/';
+const USER_NAME_URL = API_ROOT + 'username';
+
 let render = () => {
   const MainComponent = require('./components/main/main');
   const container = document.getElementById('react-root');
 
-  ReactDOM.render(<MainComponent />, container);
+  fetch(USER_NAME_URL)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      ReactDOM.render(<MainComponent userName={responseJson.userName}/>, container);
+    })
+    .catch((error) => {
+      console.log(error)
+    });
 };
 
 render();
