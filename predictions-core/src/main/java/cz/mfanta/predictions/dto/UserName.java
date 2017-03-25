4dc97ef4afb5183153069350c95f7bd3fd91776d
Martin Fanta <mafanta@cisco.com>
2017-03-24 21:03:27 -0400

    Introducing UserName entity.

---
+++ predictions-core/src/main/java/cz/mfanta/predictions/dto/UserName.java
@@ -0,0 +1,12 @@
+package cz.mfanta.predictions.dto;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @EqualsAndHashCode @Builder
+public class UserName {
+
+    private String userName;
+}
