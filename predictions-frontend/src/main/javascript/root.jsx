132e4106e25f4c4b6c9c9afe24348256d469e579
Martin Fanta <mafanta@cisco.com>
2017-03-28 20:54:34 -0400

    Switch control over from root to components.

--- predictions-frontend/src/main/javascript/root.jsx
+++ predictions-frontend/src/main/javascript/root.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const API_ROOT = 'http://localhost:8080/';
-const USER_NAME_URL = API_ROOT + 'username';
+const HelloNameContainer = require('./components/helloName/HelloNameContainer');
 
 let render = () => {
-  const MainComponent = require('./components/main/main');
   const container = document.getElementById('react-root');
 
-  fetch(USER_NAME_URL)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      ReactDOM.render(<MainComponent userName={responseJson.userName}/>, container);
-    })
-    .catch((error) => {
-      console.log(error)
-    });
+  ReactDOM.render(<HelloNameContainer/>, container);
 };
 
 render();
