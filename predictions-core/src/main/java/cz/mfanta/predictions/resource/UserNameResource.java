1a617b65412fb04663a388c85731e1e12eb2d297
Martin Fanta <mafanta@cisco.com>
2017-04-13 22:23:08 +0200

    Allow Cross-Origin requests for development purposes.

--- predictions-core/src/main/java/cz/mfanta/predictions/resource/UserNameResource.java
+++ predictions-core/src/main/java/cz/mfanta/predictions/resource/UserNameResource.java
@@ -2,10 +2,12 @@ package cz.mfanta.predictions.resource;
 
 import cz.mfanta.predictions.dto.UserName;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserNameResource {
 
     @GetMapping(
