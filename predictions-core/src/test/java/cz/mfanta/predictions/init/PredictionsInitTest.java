3912cc93e289ebfdf01a3d8e9b39e6d9e6fa0e75
Martin Fanta <mafanta@cisco.com>
2017-10-08 19:08:32 +0200

    Adding PredictionsInit unit test to increase test coverage.

---
+++ predictions-core/src/test/java/cz/mfanta/predictions/init/PredictionsInitTest.java
@@ -0,0 +1,11 @@
+package cz.mfanta.predictions.init;
+
+import org.junit.Test;
+
+public class PredictionsInitTest {
+
+    @Test
+    public void mainDoesNotThrow() throws Exception {
+        PredictionsInit.main(new String[0]);
+    }
+}
