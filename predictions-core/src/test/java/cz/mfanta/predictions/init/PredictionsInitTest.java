60d56d8b950e3e1419d5009ac6521cdb79c06bc6
Martin Fanta <mafanta@cisco.com>
2017-11-26 21:14:03 +0100

    Make PredictionsInitTest based on Spring.

--- predictions-core/src/test/java/cz/mfanta/predictions/init/PredictionsInitTest.java
+++ predictions-core/src/test/java/cz/mfanta/predictions/init/PredictionsInitTest.java
@@ -1,11 +1,20 @@
 package cz.mfanta.predictions.init;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = PredictionsInit.class)
+@ActiveProfiles("test")
 public class PredictionsInitTest {
 
     @Test
     public void mainDoesNotThrow() throws Exception {
-        PredictionsInit.main(new String[0]);
+         PredictionsInit.main(new String[0]);
     }
 }
