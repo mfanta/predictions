7556d77b1eef072e2f1c59449f10d4a6b0613698
Martin Fanta <mafanta@cisco.com>
2018-03-31 11:27:28 +0200

    Add @ToString annotation to user name.

--- predictions-core/src/main/java/cz/mfanta/predictions/dto/UserName.java
+++ predictions-core/src/main/java/cz/mfanta/predictions/dto/UserName.java
@@ -2,7 +2,7 @@ package cz.mfanta.predictions.dto;
 
 import lombok.*;
 
-@Getter @Setter @EqualsAndHashCode @Builder @AllArgsConstructor
+@Getter @Setter @EqualsAndHashCode @ToString @Builder @AllArgsConstructor
 public class UserName {
 
     private String userName;
