5a576d28f1e622652a6af12e8ae6df6d21c8da47
Martin Fanta <mafanta@cisco.com>
2018-05-01 20:02:53 +0200

    Introduce JsonFixtureReader class.

---
+++ predictions-core/src/main/java/cz/mfanta/predictions/reader/JsonFixtureReader.java
@@ -0,0 +1,14 @@
+package cz.mfanta.predictions.reader;
+
+import cz.mfanta.predictions.dto.Fixture;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonFixtureReader {
+
+    public List<Fixture> readFixtures(InputStream inputStream) {
+        return new ArrayList<>();
+    }
+}
