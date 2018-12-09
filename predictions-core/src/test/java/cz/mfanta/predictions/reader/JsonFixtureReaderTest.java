af11ea1dab2c0fcbf3fb23900aa122c8b61e9d58
Martin Fanta <mafanta@cisco.com>
2018-12-09 20:18:27 +0100

    Add unit test for JsonFixtureReader.

--- predictions-core/src/test/java/cz/mfanta/predictions/reader/JsonFixtureReaderTest.java
+++ predictions-core/src/test/java/cz/mfanta/predictions/reader/JsonFixtureReaderTest.java
@@ -1,10 +1,24 @@
 package cz.mfanta.predictions.reader;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 class JsonFixtureReaderTest {
 
+    private JsonFixtureReader reader;
+
+    @BeforeEach
+    void setUp() {
+        reader = new JsonFixtureReader();
+    }
+
     @Test
-    public void name() {
+    public void returnsEmptyFixtures() {
+        assertThat(reader.readFixtures(mock(InputStream.class))).isEmpty();
     }
 }
