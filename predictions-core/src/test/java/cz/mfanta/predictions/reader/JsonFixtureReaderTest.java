62aa530f0325c6ee3d46eebdbf2568804b38b02d
Martin Fanta <mafanta@cisco.com>
2019-01-12 08:36:28 +0100

    Make Fixture Deserialization test work.

--- predictions-core/src/test/java/cz/mfanta/predictions/reader/JsonFixtureReaderTest.java
+++ predictions-core/src/test/java/cz/mfanta/predictions/reader/JsonFixtureReaderTest.java
@@ -1,24 +1,56 @@
 package cz.mfanta.predictions.reader;
 
-import org.junit.jupiter.api.BeforeEach;
+import cz.mfanta.predictions.config.FixtureReaderConfig;
+import cz.mfanta.predictions.dto.Competition;
+import cz.mfanta.predictions.dto.Fixture;
+import cz.mfanta.predictions.dto.Fixtures;
+import cz.mfanta.predictions.dto.Team;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.time.Instant;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = FixtureReaderConfig.class)
 class JsonFixtureReaderTest {
 
+    @Autowired
     private JsonFixtureReader reader;
 
-    @BeforeEach
-    void setUp() {
-        reader = new JsonFixtureReader();
-    }
-
     @Test
-    public void returnsEmptyFixtures() {
-        assertThat(reader.readFixtures(mock(InputStream.class))).isEmpty();
+    public void readsFixtures() throws IOException {
+        try (InputStream fixturesInputStream = JsonFixtureReaderTest.class.getResourceAsStream("/sample-data/sample-fixtures.json")) {
+            Fixtures fixtures = reader.readFixtures(fixturesInputStream);
+
+            assertThat(fixtures).isNotNull();
+            List<Fixture> fixtureList = fixtures.getFixtures();
+            assertThat(fixtureList).hasSize(2);
+            assertThat(fixtureList).containsExactly(
+                    new Fixture(
+                            // Note the timezone is converted to UTC
+                            Instant.parse("2017-02-22T20:05:00.00Z"),
+                            new Team("Arsenal"),
+                            new Team("Östersunds"),
+                            new Competition("UEFA Europa League"),
+                            null // season not yet supported
+                    ),
+                    new Fixture(
+                            // Note the timezone is converted to UTC
+                            Instant.parse("2017-02-22T18:00:00.00Z"),
+                            new Team("Plzeň"),
+                            new Team("Partizan"),
+                            new Competition("UEFA Europa League"),
+                            null // season not yet supported
+                    )
+            );
+        }
     }
 }
